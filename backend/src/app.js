db276c97620e8137c7c29f4bd46ed7032ab57b5c
Piyush Tiwari <piyush.2711tiwari@gmail.com>
2025-07-30 13:16:09 +0530

    Completed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,28 +1,39 @@
-import express from 'express';
-import {createServer} from "node:http";
-import {Server} from "socket.io";   
-import mongoose from 'mongoose';
-import userRoutes from './routes/users.routes.js';
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
 
 
-import cors from 'cors';    
-import connectToSocket from './controllers/socketManager.js';
- const app = express();
- const server = createServer(app);
- const io = connectToSocket(server);
- app.set("port",(process.env.PORT || 8000));
+app.set("port", (process.env.PORT || 8000))
 app.use(cors());
-app.use(express.json({limit: '40kb'}));
-app.use(express.urlencoded({limit: '40kb',extended: true}));
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
 app.use("/api/v1/users", userRoutes);
-// app.use("/api/v2/users", newUserRoutes);
-    const start = async () => {
-        app.set("mongo_user")   
-        const connectionDb = await mongoose.connect("mongodb://localhost:27017/connectify")
-        console.log(`Connected to MongoDB:${connectionDb.connection.host}`);
-        server.listen(app.get("port"), () => {
-            console.log('Server is running on port 8000');
-        });
-    }  
-    start();
+
+const start = async () => {
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENIN ON PORT 8000")
+    });
+
+
+
+}
+
+
+
+start();
--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,29 +1,53 @@
 import React from 'react'
-export default function Landing() {
-  return (
-     <div className="landing-container">
-      <header className="header">
-        <div className="logo">Apna Video Call</div>
-        <div className="nav-links">
-          <a href="#">Join as Guest</a>
-          <a href="#">Register</a>
-          <a href="#">Login</a>
-        </div>
-      </header>
+import "../App.css"
+import { Link, useNavigate } from 'react-router-dom'
+export default function LandingPage() {
 
-      <main className="main-content">
-        <div className="text-section">
-          <h1>
-            <span className="highlight">Connect</span> with your loved Ones
-          </h1>
-          <p>Cover a distance by Apna Video Call</p>
-          <button className="get-started">Get Started</button>
-        </div>
 
-        <div className="image-section">
-          <img src="/mobile.png"  alt="Video Call Preview" />
+    const router = useNavigate();
+
+    return (
+        <div className='landingPageContainer'>
+            <nav>
+                <div className='navHeader'>
+                    <h2>Apna Video Call</h2>
+                </div>
+                <div className='navlist'>
+                    <p onClick={() => {
+                        router("/aljk23")
+                    }}>Join as Guest</p>
+                    <p onClick={() => {
+                        router("/auth")
+
+                    }}>Register</p>
+                    <div onClick={() => {
+                        router("/auth")
+
+                    }} role='button'>
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
+
+
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
+
+                    <p>Cover a distance by Apna Video Call</p>
+                    <div role='button'>
+                        <Link to={"/auth"}>Get Started</Link>
+                    </div>
+                </div>
+                <div>
+
+                    <img src="/mobile.png" alt="" />
+
+                </div>
+            </div>
+
+
+
         </div>
-      </main>
-    </div>
-  );
-};
+    )
+}
